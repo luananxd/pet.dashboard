c623a70e239c429f65779ecbdcc0db7d1c6c6285
Andrey Luzan <andrejluzan@gmail.com>
2025-02-09 00:48:49 +0700

    Добавлены моковые данные для круговой диаграммы

---
+++ src/mock/favorite-games.ts
@@ -0,0 +1,77 @@
+export default [
+  {
+    'label': 'Dota 2',
+    'values': [
+      {
+        'label': 'Achievements',
+        'title': 'achievements',
+        'value': 100,
+      },
+      {
+        'label': 'Total hours',
+        'title': 'hours',
+        'value': 201.7,
+      },
+    ],
+  },
+  {
+    'label': 'The Witcher 3: Wild Hunt',
+    'values': [
+      {
+        'label': 'Achievements',
+        'title': 'achievements',
+        'value': 60,
+      },
+      {
+        'label': 'Total hours',
+        'title': 'hours',
+        'value': 104.2,
+      },
+    ],
+  },
+  {
+    'label': 'Sleeping Dogs',
+    'values': [
+      {
+        'label': 'Achievements',
+        'title': 'achievements',
+        'value': 52,
+      },
+      {
+        'label': 'Total hours',
+        'title': 'hours',
+        'value': 49.2,
+      },
+    ],
+  },
+  {
+    'label': 'Tomb Raider',
+    'values': [
+      {
+        'label': 'Achievements',
+        'title': 'achievements',
+        'value': 100,
+      },
+      {
+        'label': 'Total hours',
+        'title': 'hours',
+        'value': 17.7,
+      },
+    ],
+  },
+  {
+    'label': 'Armello',
+    'values': [
+      {
+        'label': 'Achievements',
+        'title': 'achievements',
+        'value': 30,
+      },
+      {
+        'label': 'Total hours',
+        'title': 'hours',
+        'value': 27.1,
+      },
+    ],
+  },
+]
